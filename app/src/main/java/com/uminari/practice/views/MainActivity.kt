63642940c1aa9307cbda571e49168349d67ba022
RyujiKoyama95 <ryujikoyama95@gmail.com>
2023-01-21 14:40:44 +0900

    Add Fragment

--- app/src/main/java/com/uminari/practice/views/MainActivity.kt
+++ app/src/main/java/com/uminari/practice/views/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.uminari.practice
+package com.uminari.practice.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.uminari.practice.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
