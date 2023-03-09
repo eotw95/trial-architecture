4f0b8a960b4ae50add7579d91da54931c2672c5c
RyujiKoyama95 <ryujikoyama95@gmail.com>
2023-03-09 22:16:11 +0900

    Add create fragment observer to MainActivity

--- app/src/main/java/com/uminari/practice/todoApp/MainActivity.kt
+++ app/src/main/java/com/uminari/practice/todoApp/MainActivity.kt
@@ -6,11 +6,35 @@ package com.uminari.practice.todoApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
 import com.uminari.practice.R
+import com.uminari.practice.todoApp.viewModels.MainActivityViewModel
+import com.uminari.practice.todoApp.views.TodoListFragment
 
 class MainActivity : AppCompatActivity() {
+    private val mainActivityViewModel: MainActivityViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // observe navigateToFragment
+        mainActivityViewModel.navigateToFragment.observe(this, Observer {
+            it.getContentIfNotHandled()?.let { fragmentNavigationRequest ->
+                val transaction = supportFragmentManager.beginTransaction()
+                transaction.replace(
+                    R.id.fragmentContainer,
+                    fragmentNavigationRequest.fragment,
+                    fragmentNavigationRequest.tag
+                )
+                if (fragmentNavigationRequest.backStack) {
+                    transaction.addToBackStack(null)
+                }
+                transaction.commit()
+            }
+        })
+        val fragment = TodoListFragment.newInstance()
+        mainActivityViewModel.showFragment(fragment)
     }
 }
--- app/src/main/java/com/uminari/practice/todoApp/viewModels/MainActivityViewModel.kt
+++ app/src/main/java/com/uminari/practice/todoApp/viewModels/MainActivityViewModel.kt
@@ -4,7 +4,11 @@
 
 package com.uminari.practice.todoApp.viewModels
 
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.uminari.practice.todoApp.utils.Event
 
 /**
  * Fragmentの生成
@@ -14,5 +18,18 @@ class MainActivityViewModel: ViewModel() {
     companion object {
         private const val TAG = "MainActivityViewModel"
     }
-    
+
+    val navigateToFragment: LiveData<Event<FragmentNavigationRequest>>
+    get() = _navigateToFragment
+    private val _navigateToFragment = MutableLiveData<Event<FragmentNavigationRequest>>()
+
+    fun showFragment(fragment: Fragment, backStack: Boolean, tag: String?) {
+        _navigateToFragment.value = Event(FragmentNavigationRequest(fragment, backStack, tag)))
+    }
 }
+
+data class FragmentNavigationRequest(
+    val fragment: Fragment,
+    val backStack: Boolean = false,
+    val tag: String? = null
+)
