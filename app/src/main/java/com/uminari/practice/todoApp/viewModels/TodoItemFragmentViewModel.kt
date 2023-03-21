8cb70745f57fcf9fd2b3abb4bcf0c92ec37b9648
RyujiKoyama95 <ryujikoyama95@gmail.com>
2023-03-22 08:09:45 +0900

    Fix code for using Room

--- app/src/main/java/com/uminari/practice/todoApp/viewModels/TodoItemFragmentViewModel.kt
+++ app/src/main/java/com/uminari/practice/todoApp/viewModels/TodoItemFragmentViewModel.kt
@@ -4,36 +4,48 @@
 
 package com.uminari.practice.todoApp.viewModels
 
+import android.app.Application
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.uminari.practice.todoApp.TodoItemRepository
+import com.uminari.practice.todoApp.models.AppDatabase
 import com.uminari.practice.todoApp.models.TodoItem
 import io.realm.kotlin.where
 import io.realm.Realm
 import io.realm.kotlin.createObject
+import kotlinx.coroutines.launch
 import java.util.*
 
-class TodoItemFragmentViewModel: ViewModel() {
+class TodoItemFragmentViewModel(application: Application): ViewModel() {
     companion object {
         private const val TAG = "TodoItemFragmentVM"
     }
 
-    private val realm = Realm.getDefaultInstance()
-    private val todoItemRepository = TodoItemRepository()
+    private val todoItemRepository: TodoItemRepository
+
+    init {
+        val todoDatabase = AppDatabase.getDatabase(application)
+        val todoItemDao = todoDatabase.todoItemDao()
+        todoItemRepository = TodoItemRepository(todoItemDao)
+    }
 
     override fun onCleared() {
         Log.d(TAG, "onCleared")
         super.onCleared()
-        realm.close()
     }
 
-    fun addTodoItem(newTitle: String, newDetail: String?) {
-        Log.d(TAG, "createTodoItem newTitle=$newTitle newDetail=$newDetail")
-        todoItemRepository.insert(newTitle, newDetail)
+    fun addTodoItem(todoItem: TodoItem) {
+        viewModelScope.launch {
+            Log.d(TAG, "createTodoItem todoItem=$todoItem")
+            todoItemRepository.insert(todoItem)
+        }
     }
 
-    fun updateTodoItem(id: Long, newTitle: String, newDetail: String?) {
-        Log.d(TAG, "updateTodoItem id=$id newTitle=$newTitle newDetail=$newDetail")
-        todoItemRepository.update(id, newTitle, newDetail)
+    fun updateTodoItem(todoItem: TodoItem) {
+        viewModelScope.launch {
+            Log.d(TAG, "updateTodoItem todoItem=$todoItem")
+            todoItemRepository.update(todoItem)
+        }
     }
 }
